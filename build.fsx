a151493e4b3b9c10895b0eb4c65cf6a9cfcf7d3a
binwen <albertwoo_slaveoftime@hotmail.com>
2023-10-21 11:15:13 +0800

    WIP

--- build.fsx
+++ build.fsx
@@ -5,8 +5,8 @@ open Fun.Build
 
 pipeline "publish-to-pi" {
     stage "publish" {
-        run "dotnet publish -c Debug"
-        run "scp -r ./bin/Debug/net8.0/publish/* pi@192.168.3.48:~/Playground/Walle"
+        run "dotnet publish -c Release"
+        run "scp -r ./bin/Release/net8.0/publish/* pi@192.168.3.48:~/Playground/Walle"
     }
     runIfOnlySpecified false
 }
